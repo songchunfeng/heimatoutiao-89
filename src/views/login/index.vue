d1870f8f19a4118deb9119f993f9086560926f75
song <714514786@qq.com>
2019-12-19 19:18:11 +0800

    登录页完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,8 +6,8 @@
       <!-- 给el-form设置ref属性为了获取标签 -->
       <el-form :model="formData" :rules="loginRules" ref="myForm">
         <!-- 标签要写在el-form-item中 绑定prop属性进行校验-->
-        <el-form-item prop="moblie">
-          <el-input placeholder="请输入手机号" v-model="formData.moblie"></el-input>
+        <el-form-item prop="mobile">
+          <el-input placeholder="请输入手机号" v-model="formData.mobile"></el-input>
         </el-form-item>
         <el-form-item prop="code">
           <!-- 给标签双向绑定数据 -->
@@ -31,14 +31,14 @@ export default {
   data () {
     return {
       formData: {
-        moblie: '',
+        mobile: '',
         code: '',
         check: false
       },
       loginRules: {
         // required必填项,如果不填 就无法通过校验/如果为true,就表示该字段必填
         // pattern正则表达式
-        moblie: [{ required: true, message: '请输入手机号' }, { pattern: /^1[3456789]\d{9}$/ }],
+        mobile: [{ required: true, message: '请输入手机号' }, { pattern: /^1[3456789]\d{9}$/ }],
         code: [{ required: true, message: '请输入验证码' }, { pattern: /^\d{6}$/ }],
         // validator是自定义规则函数里面有三个值，rule当前规则，value当前表单项的值，callback 回调函数
         // 必须写三个
@@ -58,9 +58,26 @@ export default {
     // 校验整个表单方法
     submitLogin () {
       // validate 是一个方法 => 方法中传入的一个函数 两个校验参数  是否校验成功/未校验成功的字段
-      this.$refs.myForm.validate(function (isOk) {
+      this.$refs.myForm.validate(isOk => {
         if (isOk) {
-          console.log('后台传入数据')
+          console.log(this.formData)
+          // 请求后台数据
+          this.$axios({
+            url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
+            method: 'post',
+            data: this.formData
+          }).then(result => {
+            // 请求成功进入
+            // 存储token令牌方便后续请求
+            // token在resul对象中
+            window.localStorage.setItem('user-token', result.data.data.token)
+            this.$router.push('/home')
+          }).catch(() => {
+            this.$message({
+              message: '您输入的信息有误',
+              type: 'warning'
+            })
+          })
         }
       })
     }
