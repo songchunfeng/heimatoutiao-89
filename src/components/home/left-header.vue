bf8ce81ef074bff8fd0759ec5d291430e5a93fd7
song <714514786@qq.com>
2019-12-21 17:41:07 +0800

    头部

---
+++ src/components/home/left-header.vue
@@ -0,0 +1,48 @@
+<template>
+     <el-row class='layout-header' type='flex' justify='space-between' align='middle'>
+       <el-col  class='left' :span='6'>
+         <i class="el-icon-s-fold"></i>
+         <span class="title">江苏传智播客教育科技股份有限公司</span>
+       </el-col>
+       <el-col :span='4' class="right">
+         <el-row type="flex" justify="end" align='middle'>
+           <img src="../../assets/img/avatar.jpg" alt="">
+           <!-- 下拉菜单 -->
+           <el-dropdown>
+             <span>1234</span>
+             <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>个人信息</el-dropdown-item>
+                <el-dropdown-item>Git地址</el-dropdown-item>
+                <el-dropdown-item>退出</el-dropdown-item>
+             </el-dropdown-menu>
+           </el-dropdown>
+         </el-row>
+       </el-col>
+     </el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.layout-header {
+  height: 60px;
+  .left {
+    font-size: 15px;
+    .title {
+      color:#2c3e50
+    }
+  }
+  .right {
+    img {
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+      margin-right: 5px
+    }
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,31 @@
 <template>
-  <div class="home">
-    home
-  </div>
+<!-- 外层容器 -->
+  <el-container>
+    <!-- 左边栏 -->
+    <el-aside style="background-color:#323745;height:100vh;width:230px">
+      <lf-aside></lf-aside>
+    </el-aside>
+    <!-- 右侧容器 -->
+    <el-container>
+      <!-- 顶部 -->
+      <el-header>
+        <lf-header></lf-header>
+      </el-header>
+      <!-- 主体 -->
+      <el-main>身体</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 // @ is an alias to /src
+import leftAside from '../../components/home/left-aside'
+import leftHeader from '../../components/home/left-header'
 
 export default {
-
+  components: {
+    'lf-aside': leftAside,
+    'lf-header': leftHeader
+  }
 }
 </script>
