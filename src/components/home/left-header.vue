c9a07c0078292629073530b744f18266cbab975c
song <714514786@qq.com>
2019-12-21 22:14:03 +0800

    导航拦截器

--- src/components/home/left-header.vue
+++ src/components/home/left-header.vue
@@ -30,12 +30,12 @@ export default {
     }
   },
   created () {
-    let token = window.localStorage.getItem('user-token')
+    // let token = window.localStorage.getItem('user-token')
     this.$axios({
-      url: '/user/profile',
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
+      url: '/user/profile'
+      // headers: {
+      //   Authorization: `Bearer ${token}`
+      // }
     }).then(result => {
       this.useInfo = result.data.data
     })
--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './permission'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './style/index.less'
-import axios from 'axios'
+import axios from './utils/axios'
 import component from './components/index'
 
-axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 Vue.prototype.$axios = axios
 Vue.use(ElementUI)
 Vue.use(component)// 注册全局组件对象
