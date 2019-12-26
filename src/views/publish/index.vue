c52c8e945f384adafb61d2ca3567050623e9598d
song <714514786@qq.com>
2019-12-26 22:08:08 +0800

    简单发布

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,17 +1,88 @@
 <template>
-  <el-car>
-      <bread-crumb slot="header">
-        <template slot="title">发布文章</template>
-      </bread-crumb>
-  </el-car>
+  <el-card>
+    <bread-crumb slot="header">
+      <template slot="title">发布文章</template>
+    </bread-crumb>
+    <el-form style="margin-left:50px" label-width="100px" ref="myForm" :model="formData" :rules="publishRules">
+      <el-form-item label="标题" prop="title">
+        <el-input style="width:60%" v-model="formData.title"></el-input>
+      </el-form-item>
+      <el-form-item label="内容" prop="content">
+        <el-input type="textarea" v-model="formData.content"></el-input>
+      </el-form-item>
+      <el-form-item label="封面">
+        <el-radio-group v-model="formData.cover.type">
+          <el-radio :label="1">单图</el-radio>
+          <el-radio :label="3">三图</el-radio>
+          <el-radio :label="0">无图</el-radio>
+          <el-radio :label="-1">自动</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="频道" prop="channel_id">
+        <el-select value v-model="formData.channel_id">
+          <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+          <!-- 如果发布文章方法只写（）代表什么参数也不传 -->
+        <el-button type="primary" @click="publishArticle()">发布</el-button>
+        <el-button @click="publishArticle(true)">存为草稿</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      channels: [],
+      formData: {
+        title: '', // 标题校验
+        content: '', // 内容校验
+        cover: {
+          type: 0, //   封面类型 -1:自动，0-无图，1-1张，3-3张
+          images: [] // 存储的图片的地址
+        },
+        channel_id: null // 频道id
+      },
+      // 校验规则
+      publishRules: {
+        title: [{ required: true, message: '内容不能为空' }, {
+          min: 5, max: 30, message: '标题长度需要在5到30字符之间'
+        }],
+        content: [{ required: true, message: '内容不能为空' }],
+        channel_id: [{ required: true, message: '内容不能为空' }]
+      }
+    }
+  },
+  methods: {
+    // 发布文章
+    publishArticle (draft) {
+      this.$refs.myForm.validate(isOk => {
+        this.$axios({
+          url: '/articles',
+          method: 'post',
+          params: { draft },
+          data: this.formData
+        }).then(() => {
+          this.$router.push('/home/articles')
+        })
+      })
+    },
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(result => {
+        this.channels = result.data.channels
+      })
+    }
+  },
+  created () {
+    this.getChannels()
+  }
 }
 </script>
 
 <style>
-
 </style>
